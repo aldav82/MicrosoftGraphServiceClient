c24bdaba7780454ec29c6327ced0fa43b2437a03
Alfredo Alvarez <aldav@2ptr6p.onmicrosoft.com>
2023-02-23 12:09:43 -0500

    Add project files.

---
+++ MicrosoftGraph/Graph.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+namespace MicrosoftGraph
+{
+    public static class Graph
+    {
+        public static GraphServiceClient GenererClient()
+        {
+            var clientSecret = "YOUR_APP_CLIENT_SECRET";
+            var clientId = "YOUR_APP_AZURE_CLIENT_ID";
+            var tenantId = "YOUR_AZURE_TENANT_ID";
+
+            // The client credentials flow requires that you request the
+            // /.default scope, and preconfigure your permissions on the
+            // app registration in Azure. An administrator must grant consent
+            // to those permissions beforehand.
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+            var options = new TokenCredentialOptions { AuthorityHost = AzureAuthorityHosts.AzurePublicCloud };             // https://learn.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+            var clientSecretCredential = new ClientSecretCredential(
+                                tenantId,
+                                clientId,
+                                clientSecret,
+                                options);
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+            return graphClient;
+        }
+
+        public static async Task<bool> UserBelongsToGroup(this GraphServiceClient client,  string email, string groupID)
+        {
+            var users = await client.Users.Request().Filter($"userPrincipalName eq '{email}'").GetAsync();
+            var user = users.FirstOrDefault();
+            if (user == null)
+            {
+                return false;
+            }
+
+            var memberOf = await client.Users[user.Id].MemberOf.Request().GetAsync();
+            var groupIds = memberOf.Select(c => c.Id)?.ToList() ?? new List<string>();
+            while (memberOf.NextPageRequest != null)
+            {
+                memberOf = await memberOf.NextPageRequest.GetAsync();
+                groupIds.AddRange(memberOf.Select(c => c.Id).ToList());
+            }
+            return groupIds.Contains(groupID);
+        }
+    }
+}
---
+++ MicrosoftGraph/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MicrosoftGraph;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Azure Graph Client creation.....");
+var client = MicrosoftGraph.Graph.GenererClient();
+Console.WriteLine("Azure Graph Client created.....");
+
+string userName = "SOME_USER";
+string groupID = "SOME_GROUP_ID";
+
+var belongs = await client.UserBelongsToGroup(userName, groupID );
+
+if (belongs)
+{
+    Console.WriteLine($"User {userName} belongs to {groupID}");
+} else
+{
+    Console.WriteLine($"User {userName} does not belong to {groupID}");
+}
+
+Console.ReadLine();
+
+
+
+
+
